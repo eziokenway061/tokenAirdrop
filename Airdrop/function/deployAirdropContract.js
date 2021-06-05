f45950ca963f77429bea3ab958028e0bf7b97907
moondoge <75630698+eziokenway061@users.noreply.github.com>
2021-06-05 23:28:05 +0800

    add contract
    add contract

--- Airdrop/function/deployAirdropContract.js
+++ Airdrop/function/deployAirdropContract.js
@@ -1,8 +1,10 @@
 const deploy = require('./base/delpoy');
 
-const contractPath = '../contract/airdrop.sol';
+// const contractPath = '../contract/airdrop.sol';
+const contractPath = '../contract/ModoAirdrop.sol';
 
-const contractName = 'TokenAirDrop';
+// const contractName = 'TokenAirDrop';
+const contractName = 'ModoAirdrop';
 
 deploy.deployContract(contractPath,contractName,null,function (success) {
 
